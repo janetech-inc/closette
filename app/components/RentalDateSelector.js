2ef81266596fa67d7e567bb5b66ec9a8e76de0c7
Sam Hoffman <sam@Sams-MBP.fios-router.home>
2019-10-13 22:54:37 -0400

    Render date picker in modal

--- app/components/RentalDateSelector.js
+++ app/components/RentalDateSelector.js
@@ -1,34 +1,66 @@
-import { useState, useRef } from "react";
-import DatePicker from "react-datepicker";
+import { useState } from "react";
+import DatePicker, { CalendarContainer } from "react-datepicker";
 import { addDays, subDays, toDate, format } from 'date-fns'
 
 import './RentalDateSelector.scss';
+import Modal from './utils/modal/Modal';
 
 const RentalDateSelector = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(addDays(startDate, 3));
-  const datePickerRef = useRef();
+
+  const modalProps = {
+    ariaLabel: 'A label describing the Modal\'s current content',
+    role: 'dialog'
+  };
+
+  const DatePickerContainer = ({ className, children }) => {
+    return (
+      <div style={{ padding: "16px", background: "#216ba5", color: "#fff", display: "flex", justifyContent: "center" }}>
+        <CalendarContainer className={className}>
+          <div style={{ position: "relative" }}>{children}</div>
+        </CalendarContainer>
+      </div>
+    );
+  };
 
   return (
     <div className="RentalDateSelector">
       <div className="left"></div>
       <div className="center">
-        <DatePicker
-          selected={startDate}
-          onChange={date => {
-            setStartDate(date)
-            setEndDate(addDays(date, 3))
-          }}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-          value={`${format(startDate, 'MM-dd')} - ${format(endDate, 'MM-dd')}`}
-          dateFormat="MM-dd"
-          ref={datePickerRef}
-        />
+        <Modal {...modalProps} triggerText={`${format(startDate, 'MM-dd')} - ${format(endDate, 'MM-dd')}`}>
+          <DatePicker
+            selected={startDate}
+            onChange={date => {
+              setStartDate(date)
+              setEndDate(addDays(date, 3))
+            }}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            value={`${format(startDate, 'MM-dd')} - ${format(endDate, 'MM-dd')}`}
+            dateFormat="MM-dd"
+            calendarContainer={DatePickerContainer}
+            inline
+          />
+        </Modal>
       </div>
       <div className="right">
-        <button onClick={() => datePickerRef.current.setOpen(true)}>&#43;</button>
+        <Modal {...modalProps} triggerText="&#43;">
+          <DatePicker
+            selected={startDate}
+            onChange={date => {
+              setStartDate(date)
+              setEndDate(addDays(date, 3))
+            }}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            dateFormat="MM-dd"
+            calendarContainer={DatePickerContainer}
+            inline
+          />
+        </Modal>
       </div>
     </div>
   );
--- app/components/utils/modal/ModalTrigger.js
+++ app/components/utils/modal/ModalTrigger.js
@@ -1,3 +1,3 @@
-const ModalTrigger = ({buttonRef, open, text}) => <button onClick={open} ref={buttonRef}>{text}</button>;
+const ModalTrigger = ({buttonRef, open, text}) => <button onClick={open} ref={buttonRef} className="modal-trigger">{text}</button>;
 
 export default ModalTrigger;
